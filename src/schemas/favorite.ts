562aaab44e34f96a9bd2d841a9e1bd96e10c4f7e
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 00:35:04 +0100

    feat: create favorite schemas, types and transform functions

---
+++ src/schemas/favorite.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod';
+
+export const favoriteSchema = z.object({
+  id: z.number().optional(),
+  selfLink: z.string(),
+  userId: z.number(),
+});
+
+export const favoriteDbSchema = z.object({
+  id: z.number().optional(),
+  self_link: z.string(),
+  user_id: z.number(),
+});
+
+export type Favorite = z.infer<typeof favoriteSchema>;
+export type FavoriteDb = z.infer<typeof favoriteDbSchema>;
+
+export const favoriteTransform = favoriteDbSchema.transform(
+  (input): Favorite => {
+    const transformed: Favorite = {
+      selfLink: input.self_link,
+      userId: input.user_id,
+    };
+
+    if (input?.id) transformed['id'] = input.id;
+    return transformed;
+  },
+);
+
+export const favoriteDbTransform = favoriteSchema.transform(
+  (input): FavoriteDb => {
+    const transformed: FavoriteDb = {
+      self_link: input.selfLink,
+      user_id: input.userId,
+    };
+    if (input?.id) transformed['id'] = input.id;
+    return transformed;
+  },
+);
