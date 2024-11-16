53d22c795ba6ba39e660de2723260bf277a9da9a
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 00:28:00 +0100

    feat: init

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import ests from '@typescript-eslint/eslint-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  {
+    rules: {
+      curly: 'error',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: false,
+          allowTypedFunctionExpressions: false,
+          allowHigherOrderFunctions: true,
+          allowDirectConstAssertionInArrowFunctions: true,
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/no-explicit-any': ['error'],
+      'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
+      'no-magic-numbers': 'off',
+    },
+  },
+  {
+    ignores: ['node_modules'],
+  },
+  {
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+    },
+  },
+  {
+    // plugins: ['@typescript-eslint', 'prettier'],
+    plugins: {
+      prettier: prettier,
+      typescript: ests,
+    },
+  },
+  ...compat.extends('eslint:recommended'),
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  ...compat.extends('plugin:prettier/recommended'),
+];
