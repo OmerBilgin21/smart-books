9709dacd1603bff285712e8a1a12b4619b00b3f4
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 00:42:00 +0100

    feat: implement a query function that gets the whole results from google api with respect to their pagination rules

--- src/services/suggestion.service.ts
+++ src/services/suggestion.service.ts
@@ -15,3 +15,21 @@ export class SuggestionService {
     private favoriteService: FavoriteService,
   ) {}
 
+  private async queryTheWholeResult(
+    query: SearchObject[],
+  ): Promise<SuccessfulGoogleResponse[]> {
+    const initialResponse = await this.bookService.getVolumes(query);
+    const totalItems = initialResponse.totalItems;
+    const promiseArr: Promise<SuccessfulGoogleResponse>[] = [];
+
+    for (let i = 0; i < totalItems; i += 25) {
+      promiseArr.push(
+        this.bookService.getVolumes(query, { start: i, limit: 1 }),
+      );
+      delay(300);
+    }
+
+    const resolved = await Promise.all(promiseArr);
+    return [initialResponse, ...resolved];
+  }
+}
