6741c478cbe42099d9478314fa37920009d0ee70
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 00:40:35 +0100

    feat: create book types and define relevant google api result types

---
+++ src/schemas/book.ts
@@ -0,0 +1,82 @@
+export type SearchTerms = 'authors' | 'title' | 'publisher' | 'subject';
+
+export type SearchObject = {
+  term: SearchTerms;
+  value: string;
+};
+
+export type MaturityRating = 'NOT_MATURE' | 'MATURE';
+
+export type IndustryIdentifier = {
+  type: string;
+  identifier: string;
+};
+
+export type ReadingMode = {
+  text: boolean;
+  image: boolean;
+};
+
+export type VolumeInfo = {
+  title: string;
+  authors: string[];
+  publishedDate: string; // year
+  industryIdentifiers: IndustryIdentifier[];
+  description: string;
+  readingModes: ReadingMode;
+  pageCount: number;
+  printType: string;
+  categories: string[];
+  publisher?: string;
+};
+
+export type PanelizationSummary = {
+  containsEpubBubbles: boolean;
+  containsImageBubbles: boolean;
+};
+
+export type ImageLinks = {
+  smallThumbnail: string; // encoded URI
+  thumbnail: string; // encoded URI
+};
+
+export type Book = {
+  kind: string;
+  id: string;
+  etag: string;
+  selfLink: string;
+  volumeInfo: VolumeInfo;
+  saleInfo: unknown;
+  accessInfo: unknown;
+  searchInfo: unknown;
+  maturityRating?: MaturityRating;
+  allowAnonLogging?: boolean;
+  contentVersion?: string;
+  panelizationSummary?: PanelizationSummary;
+  imageLinks?: ImageLinks;
+  language?: string;
+  previewLink?: string; // nice readable preview
+  infoLink?: string; // store link
+  canonicalVolumeLink?: string;
+};
+
+export type SuccessfulGoogleResponse = {
+  kind: string;
+  totalItems: number;
+  items: Book[];
+};
+
+export enum Relevance {
+  PERFECT = 1,
+  VERY_GOOD = 2,
+  GOOD = 3,
+  MEDIOCRE = 4,
+  BAD = 5,
+  VERY_BAD = 6,
+  NO_SUGGESTION = 7,
+}
+
+export type SuggestionResult = {
+  relevance: Relevance;
+  books: Book[];
+};
