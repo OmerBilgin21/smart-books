c77445344151c86d681db8d96585c107e8a2fa75
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 00:31:27 +0100

    feat: app setup

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { APP_PORT, routes } from 'infrastructure';
+
+export const app: Express = express();
+
+app.use(
+  express.json(),
+  express.urlencoded({ extended: true }),
+  cors(),
+  cookieParser(),
+);
+
+console.info('Available Routes: ');
+routes.forEach((eachRouter): void => {
+  console.info(eachRouter.path);
+  app.use(eachRouter.path, eachRouter.router);
+});
+
+app.listen(APP_PORT, (): void => {
+  console.info(
+    `\n[server]: Server is running at http://localhost:${APP_PORT}\n`,
+  );
+});
