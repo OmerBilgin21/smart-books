c13ee65a37fbfd1500915d570cf187d708299c0c
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 00:30:34 +0100

    fix: remove unused column

---
+++ prisma/migrations/20241110025648_fix_favorites/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `usersId` on the `favorites` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "favorites" DROP COLUMN "usersId";
