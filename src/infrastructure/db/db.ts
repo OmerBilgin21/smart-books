a4a973dea47e257b94501cab2089437d5e55ae56
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 01:43:27 +0100

    feat: export dislikes table name from db util

--- src/infrastructure/db/db.ts
+++ src/infrastructure/db/db.ts
@@ -4,3 +4,4 @@ import knex from 'knex';
 export const dbClient = knex(knexConfig);
 export const USERS_TABLE = 'users';
 export const FAVORITES_TABLE = 'favorites';
+export const DISLIKES_TABLE = 'dislikes';
