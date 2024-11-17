80d382b65dfafabdb9ce906ddd4718390325ff58
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 02:57:01 +0100

    chore: export favorite categories service

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,4 @@ export * from './favorite.service';
 export * from './books.service';
 export * from './suggestion.service';
 export * from './dislike.service';
+export * from './favorite.categories.service';
