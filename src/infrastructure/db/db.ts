02c5ed5c8940c3b4c694c7457775d5febbc15eb7
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-11-17 02:56:17 +0100

    feat: export favorite categories table name from db utils

--- src/infrastructure/db/db.ts
+++ src/infrastructure/db/db.ts
@@ -5,3 +5,4 @@ export const dbClient = knex(knexConfig);
 export const USERS_TABLE = 'users';
 export const FAVORITES_TABLE = 'favorites';
 export const DISLIKES_TABLE = 'dislikes';
+export const FAVORITE_CATEGORIES_TABLE = 'favorite_categories';
