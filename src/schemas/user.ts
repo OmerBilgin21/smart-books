2cadd54a2f9a413338557075d1aa7972aeb85fdd
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-02-09 00:00:35 +0100

    feat: imrove user typing

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,3 +1,4 @@
 import { User } from '../infrastructure/db/entities/index.js';
 
-export type UserCreate = Omit<User, 'id' | 'books' | 'categories'>;
+export type PlainUser = Omit<User, 'books' | 'categories'>;
+export type UserCreate = Omit<PlainUser, 'id'>;
