b85e68aad35df3a7f5be25ed49044bc7dcf949e3
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-02-09 00:01:37 +0100

    feat: create bulk and singular create functions with uniqueness in mind

--- src/infrastructure/repositories/book.records.repository.ts
+++ src/infrastructure/repositories/book.records.repository.ts
@@ -1,34 +1,127 @@
 import { BaseRepository } from './base.repository.js';
 import { BookRecordCreate } from '../../schemas/book.record.js';
-import { BookRecord } from '../db/entities/index.js';
+import { BookRecord, User } from '../db/entities/index.js';
 import { BookRecordType } from '../db/entities/enums.js';
 
 export class BookRecordsRepository extends BaseRepository {
+  public async bulkCreate(
+    bookRecords: BookRecordCreate[],
+  ): Promise<BookRecord[]> {
+    const uniqueIds: string[] = [];
+    const uniqueCreateObjs: Omit<BookRecordCreate, 'userId'>[] = [];
+
+    const thisRepo = await this.getRepository(BookRecord);
+    const usersRepo = await this.getRepository(User);
+
+    const {
+      books: _,
+      categories: __,
+      ...user
+    } = await usersRepo.findOneOrFail({
+      where: {
+        id: bookRecords[0].userId,
+      },
+    });
+
+    if (!user) {
+      throw new Error('Given user could not be found!');
+    }
+
+    for (const bookRecord of bookRecords) {
+      if (!uniqueIds.includes(bookRecord.googleId)) {
+        uniqueIds.push(bookRecord.googleId);
+        const { userId: _userId, ...rest } = bookRecord;
+        uniqueCreateObjs.push(rest);
+      }
+    }
+
+    const qb = thisRepo.createQueryBuilder('book_records');
+
+    const rawRecords = await qb
+      .select('book_records.id')
+      .where(
+        'book_records.user = :userId AND book_records.type = :type AND book_records.googleId IN (:...googleIds)',
+        { userId: user.id, type: bookRecords[0].type, googleIds: uniqueIds },
+      )
+      .getRawMany();
+
+    const existingIds = rawRecords.map((row): string => row.id);
+    console.log('existingIds: ', existingIds);
+
+    uniqueCreateObjs.filter((record): boolean =>
+      existingIds.includes(record.googleId),
+    );
+
+    console.log('uniqueCreateObjs: ', uniqueCreateObjs);
+
+    const promises = uniqueCreateObjs.map((record): Promise<BookRecord> => {
+      const beforeCreate = {
+        ...record,
+        user,
+      };
+
+      console.log('beforeCreate: ', beforeCreate);
+
+      return thisRepo.save(beforeCreate);
+    });
+
+    return await Promise.all(promises);
+  }
+
   public async create(bookRecord: BookRecordCreate): Promise<BookRecord> {
-    const repo = await this.getRepository(BookRecord);
-    return repo.save({
-      ...bookRecord,
-      user: {
+    const thisRepo = await this.getRepository(BookRecord);
+    const usersRepo = await this.getRepository(User);
+
+    const user = await usersRepo.findOneOrFail({
+      where: {
         id: bookRecord.userId,
       },
+    });
+
+    if (!user) {
+      throw new Error('Given user could not be found!');
+    }
+
+    const existingRecord = await thisRepo.findOne({
+      where: {
+        type: bookRecord.type,
+        user: {
+          id: bookRecord.userId,
+        },
+        googleId: bookRecord.googleId,
+        selfLink: bookRecord.selfLink,
+      },
+    });
+
+    if (existingRecord) {
+      return existingRecord;
+    }
+
+    const { userId: _userId, ...rest } = bookRecord;
+
+    return thisRepo.save({
+      ...rest,
+      user,
     });
   }
 
   public async getRecordsOfTypeForUser(
-    identifier: string,
+    userId: string,
     type: BookRecordType,
   ): Promise<BookRecord[]> {
     try {
       const repo = await this.getRepository(BookRecord);
       return await repo
         .createQueryBuilder('book_records')
-        .where('book_records.user_id = :identifier ', { identifier })
-        .andWhere('book_records.type = :bookRecordType', {
-          bookRecordType: type,
-        })
+        .where(
+          'book_records.user = :identifier AND book_records.type = :bookRecordType',
+          { identifier: userId, bookRecordType: type },
+        )
         .getMany();
     } catch {
-      throw new Error('Favorites not found');
+      throw new Error(
+        `Book record for user: ${userId} not found for type: ${type}`,
+      );
     }
   }
 }
