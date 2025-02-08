22a3a19a8a668ed1d0c98b43c90debd1eb274f6d
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-02-09 00:02:03 +0100

    feat: create graceful stringifying for debugging purposes

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,2 +1,10 @@
 export const delay = (ms: number): Promise<unknown> =>
   new Promise((resolve): NodeJS.Timeout => setTimeout(resolve, ms));
+
+export const gracefullyStringfy = <T>(param: T): string | T => {
+  try {
+    return JSON.stringify(param);
+  } catch {
+    return param;
+  }
+};
