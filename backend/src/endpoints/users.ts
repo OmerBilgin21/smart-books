87c318c0280ef2615026ccbfc503d291ffae8828
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-06-29 17:22:32 +0200

    feat: add auth endpoints

--- backend/src/endpoints/users.ts
+++ backend/src/endpoints/users.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express';
 import { UsersRepository } from '../infrastructure/repositories/users.repository';
 import { UserService } from '../services/user.service';
+import { NODE_ENV } from '../infrastructure/envs';
+import { User } from '../infrastructure/db/entities';
 
 const router = Router();
 const userService = new UserService(new UsersRepository());
@@ -17,39 +19,73 @@ router.post('/', async (req: Request, res: Response): Promise<void> => {
     const created = await userService.create({
       email: data.email,
       lastName: data.lastName,
-      firstName: data.lastName,
+      firstName: data.firstName,
       password: data.password,
       suggestionIsFresh: false,
     });
 
-    res.json(created);
+    const accessToken = userService.generateAccessToken(created);
+
+    res
+      .cookie('accessToken', accessToken, {
+        httpOnly: true,
+        secure: NODE_ENV === 'production',
+        sameSite: 'strict',
+      })
+      .status(200)
+      .json(created);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error while creating user!' });
   }
 });
 
-router.get(
-  '/:identifier',
-  async (req: Request, res: Response): Promise<void> => {
-    const { identifier: emailOrId } = req.params;
+router.get('/', async (req: Request, res: Response): Promise<void> => {
+  const token = req.cookies?.accessToken;
 
-    if (!emailOrId) {
-      res.status(400).json({ error: 'Please provide an email!' });
+  if (!token) {
+    res.status(401).json({ error: 'Not Logged In!' });
+    return;
+  }
+
+  try {
+    const decodedToken = await userService.verifyToken(token);
+
+    res.json({
+      email: decodedToken.email,
+      firstName: decodedToken.firstName,
+      id: decodedToken.id,
+    });
+  } catch {
+    res
+      .status(404)
+      .json({ error: 'User not found or provided acccess token invalid!' });
+  }
+});
+
+router.get(
+  '/:email/:password',
+  async (req: Request, res: Response): Promise<void> => {
+    const { email, password } = req.params;
+
+    const validatedUser = await userService.login(email, password);
+    console.log('validatedUser: ', validatedUser);
+
+    if (validatedUser === null) {
+      res.status(401).json({ error: 'Incorrect credentials' });
       return;
     }
 
-    try {
-      const found = await userService.get(emailOrId);
+    const token = userService.generateAccessToken(validatedUser as User);
 
-      res.json({
-        email: found.email,
-        firstName: found.firstName,
-        id: found.id,
-      });
-    } catch {
-      res.status(404).json({ error: 'User not found!' });
-    }
+    res
+      .cookie('accessToken', token, {
+        httpOnly: true,
+        secure: NODE_ENV === 'production',
+        sameSite: 'strict',
+      })
+      .status(200)
+      .json(validatedUser);
   },
 );
 
