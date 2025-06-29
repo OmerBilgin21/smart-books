096de844d3fd55257e28e096fab59c053589c6b0
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-06-29 17:29:04 +0200

    feat: move landing page to non-protected ones and add signup

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -3,6 +3,7 @@ import RootLayout from "./Layout.tsx";
 import LandingPage from "@components/LandingPage";
 import Notfound from "@components/NotFound";
 import HomePage from "@components/HomePage";
+import SignUp from "@components/SignUp.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -11,16 +12,20 @@ export const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <LandingPage />,
-      },
-      {
-        path: "home",
         element: <HomePage />,
       },
     ],
   },
   {
+    path: "landing",
+    element: <LandingPage />,
+  },
+  {
     path: "*",
     element: <Notfound />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
   },
 ]);
