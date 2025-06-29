a3ca1502d261cca3b6a6bb9b20c39f5446a1b818
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-06-29 17:28:15 +0200

    feat: kick users from the root layout if not authenticated

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router";
+import { useAuth } from "@contexts/AuthContext";
+import { useNavigate } from "react-router";
 
 type Props = {};
 
 const RootLayout: React.FC<Props> = ({}: Props) => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/landing", { replace: true });
+    }
+  }, []);
+
   return (
-    <>
+    <div className="min-h-screen max-w-7xl">
       <nav>
         <Navbar />
       </nav>
       <main>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
 
