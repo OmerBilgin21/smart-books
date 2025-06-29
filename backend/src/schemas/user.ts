06729314a390f282037aceeb3d6b33a49857bc43
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-06-29 17:16:50 +0200

    feat: introduce access token type

--- backend/src/schemas/user.ts
+++ backend/src/schemas/user.ts
@@ -2,3 +2,12 @@ import { User } from '../infrastructure/db/entities/index.js';
 
 export type PlainUser = Omit<User, 'books' | 'categories'>;
 export type UserCreate = Omit<PlainUser, 'id'>;
+
+export type AccessToken = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  id: number;
+  iat: number;
+  exp: number;
+};
