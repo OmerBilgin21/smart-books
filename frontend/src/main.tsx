f7c36de7eccda7e8252e50c2a89a1d0799462e94
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-06-29 17:28:41 +0200

    feat: add toast container and wrap the app with auth context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,5 +2,13 @@ import { render } from "preact";
 import { RouterProvider } from "react-router";
 import { router } from "./router.tsx";
 import "./index.css";
+import AuthContextProvider from "@contexts/AuthProvider.tsx";
+import { ToastContainer } from "react-toastify";
 
-render(<RouterProvider router={router} />, document.getElementById("app")!);
+render(
+  <AuthContextProvider>
+    <ToastContainer />
+    <RouterProvider router={router} />
+  </AuthContextProvider>,
+  document.getElementById("app")!,
+);
