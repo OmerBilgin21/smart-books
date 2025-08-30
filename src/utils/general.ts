ac00526dc6f71a22b8c60c8b39a373b173d27567
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-08-30 22:56:03 +0200

    feat: new utils

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,13 +1,38 @@
-export const delay = (ms: number): Promise<unknown> =>
-  new Promise((resolve): NodeJS.Timeout => setTimeout(resolve, ms));
+export const delay = (s: number): Promise<unknown> =>
+  new Promise((resolve): NodeJS.Timeout => setTimeout(resolve, s * 1000));
 
 export const isNotNullish = <T>(value?: T | null): value is T => {
   return value !== undefined && value !== null;
 };
 
-export const isNullish = <T>(value?: T | null): value is T | null => {
-  return !isNotNullish(value);
+export const isNullish = <T>(value?: T | null): value is undefined | null =>
+  !isNotNullish(value);
+
+export const isEmpty = <T>(param?: T | null): boolean => {
+  if (param === undefined || param === null) {
+    return true;
+  }
+
+  if (Array.isArray(param)) {
+    return param.length === 0;
+  }
+
+  if (typeof param === 'string') {
+    return param.trim() === '';
+  }
+
+  if (param instanceof Map || param instanceof Set) {
+    return param.size === 0;
+  }
+
+  if (typeof param === 'object') {
+    return Object.keys(param).length === 0;
+  }
+
+  return false;
 };
+
+export const isNotEmpty = <T>(param?: T | null): boolean => !isEmpty(param);
 
 export const gracefullyStringfy = <T>(param: T): string | T => {
   try {
