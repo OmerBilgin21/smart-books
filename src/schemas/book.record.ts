ebbddbda8918a9b757276d87f1eb79bd12e1a9e4
OmerBilgin21 <omrfrkblgn21@gmail.com>
2025-08-31 21:13:30 +0200

    feat(book record): create proper schema with zod so entity type is not used everywhere

--- src/schemas/book.record.ts
+++ src/schemas/book.record.ts
@@ -1,5 +1,17 @@
-import { BookRecord } from '../infrastructure/db/entities/index';
+import { BookRecordType } from '../infrastructure/db/entities/enums';
+import { z } from 'zod';
 
-export type BookRecordCreate = Omit<BookRecord, 'id' | 'user'> & {
-  userId: string;
-};
+export const BookRecordSchema = z.object({
+  id: z.uuid(),
+  selfLink: z.string(),
+  googleId: z.string(),
+  type: z.enum(BookRecordType),
+  userId: z.uuid(),
+});
+
+export const BookRecordCreateSchema = BookRecordSchema.omit({
+  id: true,
+});
+
+export type BookRecord = z.infer<typeof BookRecordSchema>;
+export type BookRecordCreate = z.infer<typeof BookRecordCreateSchema>;
